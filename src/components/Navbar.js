49499b23951c2e5eb8f30c3b51cd564c3044dee6
satkar-adh <satkar147258369@gmail.com>
2024-04-24 10:47:59 +0545

    buttons added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,32 @@
 import "../App.css"
+import {useState} from "react"
 
 export default function Navbar(){
+    const [isOpen, setIsOpen] = useState(false);
+    const options = ["Course1","Course2","Course3"]
+    function handleNewsClick(){
+
+        setIsOpen(prevVal => !prevVal)
+    }
+
     return (
         <div className="navbar">
-                <div className="navelement">News</div>
-                <div className="navelement">Showcase</div>
-                <div className="navelement">Hosting</div>
-                <div className="navelement">Extend</div>
-                <div className="navelement">Learn</div>
-                <div className="navelement">Community</div>
-                <div className="navelement">About</div>
+                <button className="navelement">News</button>
+                <button className="navelement">Showcase</button>
+                <button className="navelement">Hosting</button>
+                <button className="navelement">Extend</button>
+                <button className="navelement" onClick={handleNewsClick}>Learn</button>
+                {isOpen && (
+                    <div className="Buttons">
+                    {options.map(option => (
+                        <button key={option} onClick={() => console.log(option)}>
+                            {option}
+                        </button>
+                    ))}
+                </div>
+                )}
+                <button className="navelement">Community</button>
+                <button className="navelement">About</button>
         </div>
     )
 }
