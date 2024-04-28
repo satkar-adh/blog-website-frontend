d4382d79d3622c750fc9304d6e2fab5d1ba80108
satkar-adh <satkar147258369@gmail.com>
2024-04-28 14:33:58 +0545

    Completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import Intro from './components/Intro';
 import Navbar from './components/Navbar';
-import Body from './components/Body';
+import AddPage from './crudPages/AddPage';
+import EditPage from './crudPages/EditPage';
+import EditElement from './crudPages/EditElement'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Intro />
-      <Body />
+      <Router>
+        <Routes>
+          <Route exact path="/" element= {< Intro />} />
+          <Route path="/add" element={< AddPage />} />
+          <Route path="/edit" element={< EditPage />} />
+          <Route path="/editElement/:id" element={< EditElement />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
